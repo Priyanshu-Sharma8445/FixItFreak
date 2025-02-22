f7b80081807094c8042a5fd5bc6cb492a7205c37
Priyanshu-Sharma8445 <priyanshu.sharma.mzn@gmail.com>
2025-02-22 21:21:05 +0530

    footer added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { Button } from './components/ui/button';
 import Navbar from './components/Navbar';
 import D3Element from './components/D3Element'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <Navbar/>
       <D3Element/>
+      <Footer/>
     </>
   );
 }
