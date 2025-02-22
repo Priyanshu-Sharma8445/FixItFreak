d4d00a0c1b7d5f1b0f00043bc52f0acd83c4637d
Priyanshu-Sharma8445 <priyanshu.sharma.mzn@gmail.com>
2025-02-22 21:17:01 +0530

    3d element added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import './App.css';
 import React from 'react';
 import { Button } from './components/ui/button';
 import Navbar from './components/Navbar';
+import D3Element from './components/D3Element'
 
 function App() {
   return (
     <>
       <Navbar/>
+      <D3Element/>
     </>
   );
 }
---
+++ client/src/components/D3Element.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function D3Element() {
+  return (
+    <Spline scene="https://prod.spline.design/4RmTVZdzb8rbWowM/scene.splinecode" />
+  );
+}
