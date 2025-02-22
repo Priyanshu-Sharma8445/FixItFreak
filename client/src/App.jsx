f1ceb7db0f54ac8925cfdea59d346a6a053e1ff6
Priyanshu-Sharma8445 <priyanshu.sharma.mzn@gmail.com>
2025-02-22 19:31:49 +0530

    shadcn installed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
 import React from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
   return (
     <>
-      
+      <Button>hello</Button>
     </>
   );
 }
