e39de1984da86c9279a1b34bf678d3b31c1c73e6
Manav Garg <manav2209garg@gmail.com>
2025-02-23 22:13:11 +0530

    last commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -23,11 +23,11 @@ const Navbar = () => {
 
       {/* Right: Login/Logout */}
       <div className="flex gap-3">
-        <button className="px-4 py-1 bg-blue-600 rounded-md hover:bg-blue-700">
+        <button className="px-4 py-1 bg-blue-600 rounded-md hover:bg-red-700">
           Signin
         </button>
-        <button className="px-4 py-1 bg-red-600 rounded-md hover:bg-red-700">
-          Signout
+        <button className="px-4 py-1 bg-blue-600 rounded-md hover:bg-red-700">
+          Signup
         </button>
       </div>
     </nav>
